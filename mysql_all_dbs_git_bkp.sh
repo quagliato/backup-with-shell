3f6e98bcb282aeeae7e627de39a8167d7b0910be
Eduardo Quagliato <eduardo@quagliato.me>
2015-11-11 14:15:54 -0200

    Only commits and pushes when the dump has changed.

--- mysql_all_dbs_git_bkp.sh
+++ mysql_all_dbs_git_bkp.sh
@@ -12,14 +12,26 @@ echo "BEGIN: $NOW"
 echo "------------------------------------------------------------------------------"
 echo "CREATING FILE..."
 mysqldump -h $DB_HOST --user=$DB_USER --password="${DB_PASS}" --all-databases > mysqldump.sql
-echo "MOVING FILE..."
-mv mysqldump.sql "$GIT_DIR/$BASENAME.sql"
-echo "ENTERING GIT DIR..."
-cd $GIT_DIR
-git add .
-git commit -m "$NOW"
-echo "COMMIT DONE."
-git push origin master
+head -n -1 mysqldump.sql > mysqldump-no_last_line.sql
+mv mysqldump-no_last_line.sql mysqldump.sql
+diff mysqldump.sql last_mysqldump.sql > diff.txt
+if [ -s diff.txt ] || [ ! -e last_mysqldump.sql ]
+  then
+    echo "NEW FILE DIFFERENT FROM THE LAST ONE."
+    rm diff.txt
+    cp mysqldump.sql "$GIT_DIR/$BASENAME.sql"
+    mv mysqldump.sql last_mysqldump.sql
+    echo "ENTERING GIT DIR..."
+    cd $GIT_DIR
+    git add .
+    git commit -m "$NOW"
+    echo "COMMIT DONE."
+    git push origin master
+  else
+    echo "NO CHANGES."
+    rm diff.txt
+    mv mysqldump.sql last_mysqldump.sql
+fi
 echo "------------------------------------------------------------------------------"
 END=$(date +"%Y-%m-%d_%H-%M-%S_%z")
 echo "BEGIN: $NOW"

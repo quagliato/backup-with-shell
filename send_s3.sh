edbe47ba88a49ef2f69b4d222d492fe9aece95f5
Eduardo Quagliato <eduardo@quagliato.me>
2018-02-06 12:25:19 -0200

    :hammer: updates on sending backups to AWS S3

--- send_s3.sh
+++ send_s3.sh
@@ -1,9 +1,12 @@
-clear; 
- 
+#!/bin/bash
+clear;
 NOW=$(date +"%Y-%m-%d_%H-%M-%S_%z")
 STASH_DIR=$1
 UPLOADED_DIR=$2
 BUCKET=$3
+AWS_REGION=$4
+S3KEY=$5
+S3SECRET=$6
 echo "******************************************************************************"
 echo "BEGIN: $NOW"
 echo "------------------------------------------------------------------------------"
@@ -16,26 +19,29 @@ do
   resource=`cat resource.path.new`;
   echo " ORIGIN FILE: ${file}"
   echo "DESTINY FILE: ${resource}"
-  echo "* * * * *";
   contentType="application/x-compressed-tar"
   dateValue=`date -R`
   stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
-  s3Key=XXXXXXXXXXXXXXXXXXXX # correct length
-  s3Secret=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX # correct length
-  signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+  s3Key="$S3KEY"
+  s3Secret="$S3SECRET"
+  signature=`echo -en "${stringToSign}" | openssl sha1 -hmac ${s3Secret} -binary | base64`
+
   curl -X PUT -T "${file}" \
-       -H "Host: ${BUCKET}.s3.amazonaws.com" \
+       -H "Host: ${BUCKET}.s3-$AWS_REGION.amazonaws.com" \
        -H "Date: ${dateValue}" \
        -H "Content-Type: ${contentType}" \
        -H "Authorization: AWS ${s3Key}:${signature}" \
-       https://${BUCKET}.s3.amazonaws.com/${file}
- 
-#  mv $file $UPLOADED_DIR/$file
+       https://${BUCKET}.s3-$AWS_REGION.amazonaws.com/${file}
+
+  file_new_path=`echo ${file} | cut -c 2-`;
+  mkdir -p $UPLOADED_DIR
+  mv -v $file $UPLOADED_DIR/;
   rm resource.path;
   rm resource.path.new;
+  echo "* * * * *";
 done
 echo "------------------------------------------------------------------------------"
-END=$(date +"%Y-%m-%d_%H-%M-%S_%z")
 echo "BEGIN: $NOW"
+END=$(date +"%Y-%m-%d_%H-%M-%S_%z")
 echo "  END: $END"
 echo "******************************************************************************"
